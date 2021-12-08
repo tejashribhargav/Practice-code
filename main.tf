0cd0542f0dca88cfa3407b523276d527b9c45b34
Tejashribhargav <tejashribhargav@gmail.com>
2021-12-08 14:33:48 +0530

    Push code for VM and bucket

--- main.tf
+++ main.tf
@@ -1,1 +1,26 @@
-testing
+provider "google" {
+credentials = file("credentials.json")
+project = "myproject"
+region = "us-central1"
+zone = "us-central1-c"
+}
+
+resource "google_compute_instance" "vm1" {
+name = "demo_vm"
+machine_type = "f1-micro"
+
+boot_disk {
+initialize_params {
+image = "debian-cloud/debian-9"
+}
+}
+
+network_interface {
+network = "default"
+access_config {}
+}
+}
+
+resource "google_storage_bucket" "test_bucket" {
+  name  = "my-bucket"
+}
