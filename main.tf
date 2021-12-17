b0ef3c1b131092b679f58716bd2a0d7011d7ac74
Tejashribhargav <tejashribhargav@gmail.com>
2021-12-17 19:40:23 +0530

    vpc

--- main.tf
+++ main.tf
@@ -5,22 +5,6 @@ region = "us-central1"
 zone = "us-central1-c"
 }
 
-resource "google_compute_instance" "vm1" {
-name = "demo_vm"
-machine_type = "f1-micro"
-
-boot_disk {
-initialize_params {
-image = "debian-cloud/debian-9"
-}
-}
-
-network_interface {
-network = "default"
-access_config {}
-}
-}
-
-resource "google_storage_bucket" "test_bucket" {
-  name  = "my-bucket"
+module "vpc_network" {
+  source = "vpc.tf"
 }
