dffcb4f06a485791ee9ccd8908b24343cda5e62f
tejashribhargav <tejashribhargav@gmail.com>
2021-12-20 11:11:27 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "google" {
 credentials = file("credentials.json")
 project = "qwiklabs-gcp-01-079ab02b6141"
 region = "us-central1"
-zone = "us-central1-c"
+zone = "us-central1-a"
 }
 
 module "vpc_network" {
