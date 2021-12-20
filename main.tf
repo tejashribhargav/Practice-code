e5e1b0ddacf27534be344c4eb85a5e5cc1238683
Tejashribhargav <tejashribhargav@gmail.com>
2021-12-20 12:04:57 +0530

    test

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "3.5.0"
     }
   }
 }
 provider "google" {
-  project = "qwiklabs-gcp-00-4f0a11e136f7"
-  region  = "us-central1"
-  zone    = "us-central1-c"
+  project     = "qwiklabs-gcp-03-6c41176baafb"
+  region      = "us-central1"
+  zone        = "us-central1-c"
   credentials = "credentials.json"
 }
 resource "google_compute_network" "vpc_network1" {
-  name = "terraform-network1"
+  name = "terraform-network2"
 }
