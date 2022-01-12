8fa2790d8d855ed2081516ab37cd172e5625f557
Tejashribhargav <tejashribhargav@gmail.com>
2022-01-12 18:07:57 +0530

    update

---
+++ variable.tf
@@ -0,0 +1,16 @@
+variable "project" {
+  default = "qwiklabs-gcp-03-6c41176baafb"
+}
+
+variable "region" {
+    default = "us-central-a"
+}
+
+variable "zone" {
+    default = "us-central-a"
+}
+
+variable "credentials" {
+    default = "credentials.json"
+}
+
---
+++ vpc.tf
@@ -0,0 +1,7 @@
+
+/*
+resource "google_compute_subnetwork" "subnetwork" {
+name = "terraform-subnetwork"
+ip_cidr_range = "10.2.0.0/16"
+network = google_compute_network.vpc_network.name
+}*/
